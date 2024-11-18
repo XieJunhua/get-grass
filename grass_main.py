4a7b85b90ce2a858b0760f6dae304f8ff0334563
XieJunhua <xiejunhua2008@gmail.com>
2024-11-19 00:25:56 +0800

    add proxy

--- grass_main.py
+++ grass_main.py
@@ -7,8 +7,11 @@ import random
 import time
 import logging
 
+
 def setup_logging():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
 
 
 # function to handle cookie banner: If a cookie banner is present press the button containing the accept text
@@ -20,72 +23,87 @@ def handle_cookie_banner(driver):
         driver (webdriver): The WebDriver instance.
     """
     try:
-        cookie_banner = driver.find_element(By.XPATH, "//button[contains(text(), 'ACCEPT')]")
+        cookie_banner = driver.find_element(
+            By.XPATH, "//button[contains(text(), 'ACCEPT')]"
+        )
         if cookie_banner:
-            logging.info('Cookie banner found. Accepting cookies...')
+            logging.info("Cookie banner found. Accepting cookies...")
             cookie_banner.click()
             time.sleep(random.randint(3, 11))
-            logging.info('Cookies accepted.')
+            logging.info("Cookies accepted.")
     except Exception:
         pass
 
 
 def run():
     setup_logging()
-    logging.info('Starting the script...')
+    logging.info("Starting the script...")
 
     # Read variables from the OS env
-    email = os.getenv('GRASS_USER')
-    password = os.getenv('GRASS_PASS')
-    extension_id = os.getenv('EXTENSION_ID')
-    extension_url = os.getenv('EXTENSION_URL')
+    email = os.getenv("GRASS_USER")
+    password = os.getenv("GRASS_PASS")
+    extension_id = os.getenv("EXTENSION_ID")
+    extension_url = os.getenv("EXTENSION_URL")
+    proxy_host = os.getenv("PROXY_HOST")
+    proxy_port = os.getenv("PROXY_PORT")
+    proxy_user = os.getenv("PROXY_USER")
+    proxy_pass = os.getenv("PROXY_PASS")
 
     # Check if credentials are provided
     if not email or not password:
-        logging.error('No username or password provided. Please set the GRASS_USER and GRASS_PASS environment variables.')
+        logging.error(
+            "No username or password provided. Please set the GRASS_USER and GRASS_PASS environment variables."
+        )
         return  # Exit the script if credentials are not provided
 
     chrome_options = Options()
-    chrome_options.add_extension(f'./{extension_id}.crx')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless=new')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0")
+    chrome_options.add_extension(f"./{extension_id}.crx")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument(
+        "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"
+    )
+    chrome_options.add_argument(
+        f"--proxy-server=http://{proxy_user}:{proxy_pass}@{proxy_host}:{proxy_port}"
+    )
 
     # Initialize the WebDriver
     driver = webdriver.Chrome(options=chrome_options)
+    ipinfo = driver.get("http://ip-api.com/json")  # Or any other IP check service
+    logging.info(f"Proxy connection successful: {ipinfo}")
 
     try:
         # Navigate to a webpage
-        logging.info(f'Navigating to {extension_url} website...')
+        logging.info(f"Navigating to {extension_url} website...")
         driver.get(extension_url)
-        time.sleep(random.randint(3,7))
+        time.sleep(random.randint(3, 7))
         handle_cookie_banner(driver)
-        logging.info('Entering credentials...')
-        username = driver.find_element(By.NAME,"user")
+        logging.info("Entering credentials...")
+        username = driver.find_element(By.NAME, "user")
         username.send_keys(email)
-        passwd = driver.find_element(By.NAME,"password")
+        passwd = driver.find_element(By.NAME, "password")
         passwd.send_keys(password)
-        
-        logging.info('Clicking the login button...')
+
+        logging.info("Clicking the login button...")
         button = driver.find_element(By.XPATH, "//button")
         button.click()
-        logging.info('Waiting response...')
+        logging.info("Waiting response...")
 
-        time.sleep(random.randint(10,50))
-        logging.info('Accessing extension settings page...')
-        driver.get(f'chrome-extension://{extension_id}/index.html')
-        time.sleep(random.randint(3,7))
+        time.sleep(random.randint(10, 50))
+        logging.info("Accessing extension settings page...")
+        driver.get(f"chrome-extension://{extension_id}/index.html")
+        time.sleep(random.randint(3, 7))
 
-        logging.info('Clicking the extension button...')
+        logging.info("Clicking the extension button...")
         button = driver.find_element(By.XPATH, "//button")
         button.click()
 
-        logging.info('Logged in successfully.')
+        logging.info("Logged in successfully.")
         handle_cookie_banner(driver)
-        logging.info('Earning...')
+        logging.info("Earning...")
     except Exception as e:
-        logging.error(f'An error occurred: {e}')
+        logging.error(f"An error occurred: {e}")
         driver.quit()
         time.sleep(60)
         run()
@@ -94,8 +112,9 @@ def run():
         try:
             time.sleep(3600)
         except KeyboardInterrupt:
-            logging.info('Stopping the script...')
+            logging.info("Stopping the script...")
             driver.quit()
             break
+
 
 run()
